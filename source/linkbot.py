dd96cdbceee1159bca66d2baae7854ad35b486e4
John Mercouris <john@mercouris.email>
2022-04-20 23:46:51 -0500

    linkbot.py: listen for message events

--- source/linkbot.py
+++ source/linkbot.py
@@ -2,6 +2,8 @@
 
 import configparser
 import logging
+import discord
+
 
 # Set the logging up
 logging.basicConfig(level=logging.INFO)
@@ -12,3 +14,22 @@ configuration.read('configuration.ini')
 
 # Get needed values from the configuration.ini file
 token = configuration.get('discord', 'token')
+
+# Create a discord client
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    """Invoke when the bot has connected to Discord."""
+    print(f'{client.user} has connected to Discord!')
+
+
+@client.event
+async def on_message(message):
+    """Invoke when a message is received on the Guild/server."""
+    print(message.content)
+
+
+if __name__ == '__main__':
+    client.run(token)
