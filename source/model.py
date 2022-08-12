98b7b95d5885203edd39587deb065e4322b37db0
John Mercouris <john@mercouris.email>
2022-08-12 00:26:48 -0500

    model.py: create a model for each channel

--- source/model.py
+++ source/model.py
@@ -9,6 +9,26 @@ from sqlalchemy.types import TIMESTAMP
 Base = declarative_base()
 
 
+class Channel(Base):
+    """This class represents a channel on a server."""
+
+    __tablename__ = 'channel'
+    id = Column(Integer, primary_key=True)
+    channel_id = Column(Text)
+    server_id = Column(Text)
+    block_links_p = Column(Boolean)
+
+    def __init__(self, channel_id, server_id):
+        """Create a Channel.
+
+        :param channel_id: The Discord channel id
+        :param server_id: The Discord guild/server id
+        :returns: nil
+        """
+        self.channel_id = channel_id
+        self.server_id = server_id
+
+
 class AllowDomain(Base):
     """This class represents a hostname (domain) to allow on a server."""
 
