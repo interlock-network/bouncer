d919cbe9690a46f5651757ca335b5c8884483694
John Mercouris <john@mercouris.email>
2022-08-16 02:14:04 -0500

    discord_logger.py: initialize skeleton

---
+++ source/discord_logger.py
@@ -0,0 +1,9 @@
+"""This module provides a function to log to a Discord channel."""
+
+import logging
+
+
+class DiscordLogger(logging.Handler):
+    """This class is responsible for logging to Discord."""
+
+    pass
