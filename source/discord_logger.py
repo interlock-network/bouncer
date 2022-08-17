793c514697b0019ca47303eb1f0ce59a75710fd9
John Mercouris <john@mercouris.email>
2022-08-16 23:36:43 -0500

    discord_logger.py: create log_channels function to fetch channels

--- source/discord_logger.py
+++ source/discord_logger.py
@@ -1,12 +1,28 @@
 """This module provides a function to log to a Discord channel."""
 
 import logging
+from utility import client
+from utility import configuration
 
 
 class DiscordLogger(logging.Handler):
     """This class is responsible for logging to Discord."""
 
+    def log_channels(self):
+        """Return the list of channels to log to."""
+        if hasattr(self, "channels") and self.channels != []:
+            return self.channels
+        else:
+            self.channels = []
+            for guild in client.guilds:
+                for channel in guild.channels:
+                    if channel.name == configuration.get(
+                            'configuration', 'channel_log'):
+                        self.channels.append(channel)
+            return self.channels
+
     def emit(self, record):
         """Invoke when a log is to be logged."""
         msg = self.format(record)
+        print(self.log_channels())
         print(msg)
