1c1d8eb0f71a7fc964034287803bd4a6d42e48ce
John Mercouris <john@mercouris.email>
2022-09-23 02:23:39 -0500

    bouncer.py: add a settings page the user can edit the settings for bouncer

--- source/bouncer.py
+++ source/bouncer.py
@@ -1,21 +1,22 @@
 """Main entry point for the Interlock-Bouncer."""
 
-import gettext
 import logging
 
+from gettext import gettext
 from urllib.parse import urlparse
-from model import AllowDomain, Message, Channel
+from model import AllowDomain, Message, Channel, SettingsAccessRequest
 from model import find_or_create_channel
 from utility import urls_from_str, session, bot, token, configuration, MESSAGE
+from utility import max_url_length, bouncer_domain
 from predicates import (url_http_p, url_malicious_p, allow_url_p,
                         str_contains_url_p)
 from discord_logger import DiscordLogger
+from flask import Flask, request, render_template
+from secrets import token_urlsafe
+from threading import Thread
 
 # Setup gettext for i18n
-_ = gettext.gettext
-
-# Max URL length
-max_url_length = configuration.getint('configuration', 'max_url_length')
+_ = gettext
 
 # Set the logging up
 logging.basicConfig(filename=configuration.get('configuration', 'log_file'),
@@ -24,6 +25,48 @@ logger = logging.getLogger()
 
 discord_logger = DiscordLogger()
 logger.addHandler(discord_logger)
+
+app = Flask(__name__)
+
+
+@app.route('/settings', methods=['GET'])
+def settings_view():
+    """Return a page the user can edit settings on."""
+    key = request.args.get('key')
+    access_request = session.query(SettingsAccessRequest).filter_by(key=key).first()
+    if access_request:
+        return render_template('settings.html',
+                               key=key,
+                               server_name=access_request.server_name,
+                               channel_name=access_request.channel_name)
+    else:
+        return "Access forbidden."
+
+
+@app.route('/settings', methods=['POST'])
+def settings_save():
+    """Save the user requested settings."""
+    key = request.form.get("key")
+    access_request = session.query(SettingsAccessRequest).filter_by(key=key).first()
+    if access_request:
+        channel = find_or_create_channel(access_request.channel_id, access_request.server_id)
+
+        if(request.form.get("urls_disabled")):
+            channel.block_links_p = False
+        else:
+            channel.block_links_p = True
+
+        for url in request.form.get("allowed_urls").split(","):
+            url_object = urlparse(url)
+            session.add(AllowDomain(url_object.hostname, access_request.server_id))
+
+        session.query(SettingsAccessRequest).filter_by(key=key).delete()
+        session.commit()
+        return render_template("settings_save.html",
+                               server_name=access_request.server_name,
+                               channel_name=access_request.channel_name)
+    else:
+        return "Access forbidden."
 
 
 @bot.event
@@ -84,6 +127,19 @@ async def process_message(message):
             break
         # If we have made it to this point, URL is OK
         logger.log(MESSAGE, "URL marked as secure: %s", url, extra={'server': message.guild})
+
+
+@bot.slash_command()
+async def edit_settings_web_interface(ctx):
+    """Allow the user to edit channel settings via a web interface."""
+    key = token_urlsafe(32)
+    session.add(SettingsAccessRequest(key, ctx.guild.id, ctx.channel.id,
+                                      ctx.guild.name, ctx.channel.name))
+    session.commit()
+    await ctx.author.send(
+        "To edit the settings for channel `{}` in server `{}`, visit: {}/settings?key={} in a browser. This URL can only be used once!".format(
+            ctx.channel.name, ctx.guild.name, bouncer_domain, key))
+    await ctx.respond("Edit code successfully requested. Please check your direct messages.")
 
 
 @bot.slash_command()
@@ -150,4 +206,6 @@ async def on_message_edit(message_before, message_after):
     await process_message(message_after)
 
 if __name__ == '__main__':
+    # Start Flask on a separate thread.
+    Thread(target=lambda: app.run(debug=True, use_reloader=False)).start()
     bot.run(token)
