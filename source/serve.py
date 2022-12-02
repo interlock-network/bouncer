b410da0819efd56e47975839e5cf8a83f4af0488
Dan TS <dan@interlock.network>
2022-12-02 14:08:06 -0800

    adds API info to show pipeline stats for total and malicious URLs

--- source/serve.py
+++ source/serve.py
@@ -14,6 +14,7 @@ app = Flask(__name__)
 @app.route('/statistics', methods=['GET'])
 def statistics_view():
     """Return a view of KPIs."""
+    pipeline_base_url = "http://octahedron.interlock.network"
     server_count = len(bot.guilds)
     user_count = 0
     for server in bot.guilds:
@@ -23,25 +24,36 @@ def statistics_view():
         "key": backend_api_key,
     }
 
-    urls_scanned_count = requests.post("{0}/urls_scanned_count".format(backend_base_url), json=json_payload).text
-    malicious_urls_scanned_count = requests.post("{0}/malicious_urls_scanned_count".format(backend_base_url), json=json_payload).text
-    unique_urls_scanned_count = requests.post("{0}/unique_urls_scanned_count".format(backend_base_url), json=json_payload).text
-    unique_malicious_urls_scanned_count = requests.post("{0}/unique_malicious_urls_scanned_count".format(backend_base_url), json=json_payload).text
+    malicious_urls_scanned_count = requests.post(
+        "{0}/malicious_urls_scanned_count".format(backend_base_url), json=json_payload).text
+    total_malicious_urls_scanned_count = requests.post(
+        "{0}/malicious_urls_scanned_count".format(pipeline_base_url), json=json_payload).text
+    total_urls_scanned_count = requests.post(
+        "{0}/urls_scanned_count".format(pipeline_base_url), json=json_payload).text
+    unique_malicious_urls_scanned_count = requests.post(
+        "{0}/unique_malicious_urls_scanned_count".format(backend_base_url), json=json_payload).text
+    unique_urls_scanned_count = requests.post(
+        "{0}/unique_urls_scanned_count".format(backend_base_url), json=json_payload).text
+    urls_scanned_count = requests.post(
+        "{0}/urls_scanned_count".format(backend_base_url), json=json_payload).text
 
     return render_template('statistics.html',
-                           server_count=server_count,
-                           user_count=user_count,
-                           urls_scanned_count=urls_scanned_count,
                            malicious_urls_scanned_count=malicious_urls_scanned_count,
+                           server_count=server_count,
+                           total_malicious_urls_scanned_count=total_malicious_urls_scanned_count,
+                           total_urls_scanned_count=total_urls_scanned_count,
+                           unique_malicious_urls_scanned_count=unique_malicious_urls_scanned_count,
                            unique_urls_scanned_count=unique_urls_scanned_count,
-                           unique_malicious_urls_scanned_count=unique_malicious_urls_scanned_count)
+                           urls_scanned_count=urls_scanned_count,
+                           user_count=user_count)
 
 
 @app.route('/settings', methods=['GET'])
 def settings_view():
     """Return a page the user can edit settings on."""
     key = request.args.get('key')
-    access_request = session.query(SettingsAccessRequest).filter_by(key=key).first()
+    access_request = session.query(
+        SettingsAccessRequest).filter_by(key=key).first()
     if access_request:
         return render_template('settings.html',
                                key=key,
@@ -55,9 +67,11 @@ def settings_view():
 def settings_save():
     """Save the user requested settings."""
     key = request.form.get("key")
-    access_request = session.query(SettingsAccessRequest).filter_by(key=key).first()
+    access_request = session.query(
+        SettingsAccessRequest).filter_by(key=key).first()
     if access_request:
-        channel = find_or_create_channel(access_request.channel_id, access_request.server_id)
+        channel = find_or_create_channel(
+            access_request.channel_id, access_request.server_id)
         urls_disabled = request.form.get("urls_disabled")
         allowed_urls = request.form.get("allowed_urls")
 
@@ -68,7 +82,8 @@ def settings_save():
 
         for url in allowed_urls.split(","):
             url_object = urlparse(url)
-            session.add(AllowDomain(url_object.hostname, access_request.server_id))
+            session.add(AllowDomain(url_object.hostname,
+                        access_request.server_id))
 
         session.query(SettingsAccessRequest).filter_by(key=key).delete()
         session.commit()
