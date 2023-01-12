c2a3740e80161d1624d5b5de9de7e82c8bd0fc0d
John Mercouris <john@mercouris.email>
2023-01-12 16:50:31 -0600

    serve.py: eliminate /statistics view, expose values in endpoint

--- source/serve.py
+++ source/serve.py
@@ -11,32 +11,6 @@ from urllib.parse import urlparse
 app = Flask(__name__)
 
 
-@app.route('/statistics', methods=['GET'])
-def statistics_view():
-    """Return a view of KPIs."""
-    server_count = len(bot.guilds)
-    user_count = 0
-    for server in bot.guilds:
-        user_count += server.member_count
-
-    json_payload = {
-        "key": backend_api_key,
-    }
-
-    urls_scanned_count = requests.post("{0}/urls_scanned_count".format(backend_base_url), json=json_payload).text
-    malicious_urls_scanned_count = requests.post("{0}/malicious_urls_scanned_count".format(backend_base_url), json=json_payload).text
-    unique_urls_scanned_count = requests.post("{0}/unique_urls_scanned_count".format(backend_base_url), json=json_payload).text
-    unique_malicious_urls_scanned_count = requests.post("{0}/unique_malicious_urls_scanned_count".format(backend_base_url), json=json_payload).text
-
-    return render_template('statistics.html',
-                           server_count=server_count,
-                           user_count=user_count,
-                           urls_scanned_count=urls_scanned_count,
-                           malicious_urls_scanned_count=malicious_urls_scanned_count,
-                           unique_urls_scanned_count=unique_urls_scanned_count,
-                           unique_malicious_urls_scanned_count=unique_malicious_urls_scanned_count)
-
-
 @app.route('/settings', methods=['GET'])
 def settings_view():
     """Return a page the user can edit settings on."""
@@ -56,6 +30,11 @@ def user_count():
     for server in bot.guilds:
         user_count += server.member_count
     return str(user_count)
+
+@app.route('/server_count', methods=['GET'])
+def server_count():
+    server_count = len(bot.guilds)
+    return str(server_count)
 
 @app.route('/settings', methods=['POST'])
 def settings_save():
