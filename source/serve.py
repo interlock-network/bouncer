66be75caaadf2ae1e8c59d5f09d4641863ed8fa8
John Mercouris <john@mercouris.email>
2023-01-13 01:33:39 -0600

    serve.py: protect sensitive routes via API key

--- source/serve.py
+++ source/serve.py
@@ -1,14 +1,35 @@
 """Entry point for the Interlock web server."""
 
 import requests
+from functools import wraps
 from flask import Flask, request, render_template
 from utility import bot, session
-from model import SettingsAccessRequest, AllowDomain
+from model import SettingsAccessRequest, AllowDomain, APIKey
 from model import find_or_create_channel
 from urllib.parse import urlparse
 
 
 app = Flask(__name__)
+
+
+def require_key(f):
+    """
+    Decorate all requests to require keys.
+
+    @param f: flask function.
+    @return: decorator, return the wrapped function or abort json object.
+    """
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        request_data = request.get_json()
+        key = request_data['key']
+        if bool(session.query(APIKey).filter_by(key=key).first()):
+            return f(*args, **kwargs)
+        else:
+            logger.warning("Unauthorized address trying to use API: " +
+                           request.remote_addr)
+            abort(401)
+    return decorated
 
 
 @app.route('/settings', methods=['GET'])
@@ -24,17 +45,6 @@ def settings_view():
     else:
         return "Access forbidden."
 
-@app.route('/user_count', methods=['GET'])
-def user_count():
-    user_count = 0
-    for server in bot.guilds:
-        user_count += server.member_count
-    return str(user_count)
-
-@app.route('/server_count', methods=['GET'])
-def server_count():
-    server_count = len(bot.guilds)
-    return str(server_count)
 
 @app.route('/settings', methods=['POST'])
 def settings_save():
@@ -62,3 +72,19 @@ def settings_save():
                                channel_name=access_request.channel_name)
     else:
         return "Access forbidden."
+
+
+@app.route('/user_count', methods=['POST'])
+@require_key
+def user_count():
+    user_count = 0
+    for server in bot.guilds:
+        user_count += server.member_count
+    return str(user_count)
+
+
+@app.route('/server_count', methods=['POST'])
+@require_key
+def server_count():
+    server_count = len(bot.guilds)
+    return str(server_count)
