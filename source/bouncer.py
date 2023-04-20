6331098bab64a563c646e5fd19c8eedc2b2786e8
John Mercouris <john@mercouris.email>
2023-04-20 12:48:17 -0500

    bouncer: strip server functionality.

--- source/bouncer.py
+++ source/bouncer.py
@@ -5,17 +5,16 @@ import logging
 import discord
 from gettext import gettext
 from urllib.parse import urlparse
-from model import AllowDomain, Message, Channel, SettingsAccessRequest
+from model import AllowDomain, Message, Channel
 from model import find_or_create_channel
 from utility import urls_from_str, session, bot, token, configuration, MESSAGE
-from utility import max_url_length, bouncer_domain
+from utility import max_url_length
 from predicates import (url_http_p, url_malicious_p, allow_url_p,
                         str_contains_url_p)
 from discord_logger import DiscordLogger
 
 from secrets import token_urlsafe
 from threading import Thread
-from serve import app
 
 # Setup gettext for i18n
 _ = gettext
@@ -95,21 +94,6 @@ async def process_message(message):
         logger.log(MESSAGE, "Scanned safe URL: `%s`\nChannel: `#%s`",
                    url, message.channel.name,
                    extra={'server': message.guild})
-
-
-@bot.slash_command()
-async def web_channel_edit(ctx):
-    """Allow the user to edit channel settings via a web interface."""
-    if not ctx.author.guild_permissions.ban_members:
-        await ctx.respond("Denied! You're not authorized to do this.")
-        return
-    key = token_urlsafe(32)
-    session.add(SettingsAccessRequest(key, ctx.guild.id, ctx.channel.id,
-                                      ctx.guild.name, ctx.channel.name))
-    session.commit()
-    await ctx.respond(
-        "Yo! To edit Bouncer settings for channel `#{}`, click {}/settings?key={}. This URL can only be used once!".format(
-            ctx.channel.name, bouncer_domain, key), ephemeral=True)
 
 
 @bot.slash_command()
@@ -221,7 +205,4 @@ async def on_message_edit(message_before, message_after):
     await process_message(message_after)
 
 if __name__ == '__main__':
-    # Start Flask on a separate thread.
-    Thread(target=lambda: app.run(debug=False,
-           use_reloader=False, host='0.0.0.0', port=3000)).start()
     bot.run(token)
